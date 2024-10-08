58c8b5859798f43867c1a13463986a548c1a20a6
aleiram19 <a23aleminram@inspedralbes.cat>
2024-10-08 10:33:29 +0200

    TrabajoFinal

--- back/listarPreguntes.php
+++ back/listarPreguntes.php
@@ -8,37 +8,3 @@ $result = $conn->query($sql);
 if (!$result) {
     die("Error en la consulta: " . $conn->error);
 }
-
-// GENERO MI HTML
-?>
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title>Lista de Preguntas</title>
-</head>
-<body>
-    <h1>Lista de Preguntas</h1>
-    <button id="volverInicio">Volver al Inicio</button>
-
-    <div class="table-container">
-        <table>
-            <tr>
-                <th>ID</th>
-                <th>Pregunta</th>
-                <th>Acciones</th>
-            </tr>
-            <?php while ($row = $result->fetch_assoc()): ?>
-            <tr>
-                <td><?php echo $row['id']; ?></td>
-                <td><?php echo $row['pregunta']; ?></td>
-                <td>
-                    <button class="edit-button" data-id="<?php echo $row['id']; ?>">Editar</button>
-                    <button class="delete-button" data-id="<?php echo $row['id']; ?>">Eliminar</button>
-                </td>
-            </tr>
-            <?php endwhile; ?>
-        </table>
-    </div>
-</body>
-</html>
