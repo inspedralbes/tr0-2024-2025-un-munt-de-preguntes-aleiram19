7b1a254f77b55496da14fd39625e87497aa445a2
aleiram19 <a23aleminram@inspedralbes.cat>
2024-10-08 10:42:30 +0200

    Trabajo Final

--- back/conexio.php
+++ back/conexio.php
@@ -1,15 +1,15 @@
 <?php
 //PRODUCCION
-// $server = "localhost:3306"; 
-// $username = "a23aleminram_aleiram";
-// $password = "Ramos190598@";
-// $bd = "a23aleminram_quiz"; 
+$server = "localhost:3306"; 
+$username = "a23aleminram_aleiram";
+$password = "Ramos190598@";
+$bd = "a23aleminram_quiz"; 
 
 // LOCAL
-$server = "localhost"; 
-$username = "root"; 
-$password = ""; 
-$bd = "quiz"; 
+// $server = "localhost"; 
+// $username = "root"; 
+// $password = ""; 
+// $bd = "quiz"; 
 
 $conn = new mysqli($server, $username, $password, $bd);
 
